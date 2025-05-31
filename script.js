737061af0e541522ec925b844b6e42b96f2689da
Duckycolors <codeandwork23@protonmail.com>
2025-05-31 13:05:40 +1000

    Initial choice startup

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello World!");
+
+let randomChoice = Math.random();
+console.log(randomChoice);
+
+function getComputerChoice() {
+    if (randomChoice > 0.33) {
+        return "Paper";
+    } else if (randomChoice > 0.66) {
+        return "Scissors";
+    } else {
+        return "Rock";
+    }
+}
+
+getComputerChoice();
+
+function getHumanChoice() {
+    humanChoice = prompt("Rock, Paper or Scissors?");
+    console.log(humanChoice);
+}
+
+getHumanChoice();
