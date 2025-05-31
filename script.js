55fbd21d451e62fe29cfcfa3ae03d087ea6b16f4
Duckycolors <codeandwork23@protonmail.com>
2025-05-31 13:18:13 +1000

    Computer choice working

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-console.log("Hello World!");
+let humanScore = 0;
+let computerScore = 0;
+
 
 let randomChoice = Math.random();
 console.log(randomChoice);
 
 function getComputerChoice() {
-    if (randomChoice > 0.33) {
+    if (randomChoice > 0.66) {
         return "Paper";
-    } else if (randomChoice > 0.66) {
+    } else if (randomChoice < 0.66 && randomChoice > 0.33) {
         return "Scissors";
     } else {
         return "Rock";
     }
 }
-
-getComputerChoice();
+console.log(getComputerChoice());
 
 function getHumanChoice() {
     humanChoice = prompt("Rock, Paper or Scissors?");
     console.log(humanChoice);
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+playRound(humanSelection, computerSelection);
