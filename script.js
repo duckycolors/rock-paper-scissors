523d5d722b725e3cf2aee23d0642e11f2a0264cc
Duckycolors <codeandwork23@protonmail.com>
2025-05-31 14:11:50 +1000

    playRound finish logic

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ let computerScore = 0;
 function getComputerChoice() {
     let computerChoice = Math.random();
     if (computerChoice > 0.66) {
+        computerChoice = "paper";
+        console.log(computerChoice);
         return "paper";
     } else if (computerChoice < 0.66 && computerChoice > 0.33) {
+        computerChoice = "scissors";
+        console.log(computerChoice);
         return "scissors";
     } else {
+        computerChoice = "rock";
+        console.log(computerChoice);
         return "rock";
     }
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper or Scissors?");
@@ -22,21 +27,26 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock" &&  computerChoice === "paper"){
+        computerScore++;
         return console.log("You lose! " + computerChoice + " beats " + humanChoice);
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
         return console.log("You lose! " + computerChoice + " beats " + humanChoice);
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
         return console.log("You lose! " + computerChoice + " beats " + humanChoice);
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        return console.log("You win! " + computerChoice + " beats " + humanChoice);
+        humanScore++;
+        return console.log("You win! " + humanChoice + " beats " + computerChoice);
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        return console.log("You win! " + computerChoice + " beats " + humanChoice);
+        humanScore++;
+        return console.log("You win! " + humanChoice + " beats " + computerChoice);
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        return console.log("You win! " + computerChoice + " beats " + humanChoice);
+        humanScore++;
+        return console.log("You win! " + humanChoice + " beats " + computerChoice);
     } else {
         return console.log("Tie! Both are the same.");
     }
-      
 }
 
 const humanSelection = getHumanChoice();
